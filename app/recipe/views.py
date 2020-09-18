3ffa195f0b58487bf3111bdc2bffa5ceaf76b952
CarloSantovito <carlo.santovito@travelperk.com>
2020-09-18 17:55:17 +0200

    Added create recipe feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -45,3 +45,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
         return self.queryset.filter(user=self.request.user)
+
+    # If you are getting the recipe Detail serializer must be changed
+    # in RecipeDetailSerializer
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
